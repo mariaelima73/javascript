245d5f52f470a568238fcd69ffd36a7f9395a005
maria.e.lima73 <maria.e.lima73@aluno.senai.br>
2024-04-11 10:47:18 -0300

    Dia 10

---
+++ Condicionais/exercício2.js
@@ -0,0 +1,10 @@
+// console.log('O console funcionou corretamente.')
+var país = "Canadá";
+console.log(`Vivendo no ${país}`)
+if (país == 'Brasil'){
+    console.log('você é brasileiro.')
+}
+else{
+    console.log('você é estrangeiro.')
+}
+
