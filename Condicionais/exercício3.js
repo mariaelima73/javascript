681d0c3d24d952f403f041d339473da858df8754
maria.e.lima73 <maria.e.lima73@aluno.senai.br>
2024-04-12 10:47:06 -0300

    Dia 11

---
+++ Condicionais/exercício3.js
@@ -0,0 +1,11 @@
+var idade = 65
+if (idade < 16){
+    console.log('Você não está apto para votar.')
+} else if (idade >= 16 && idade < 18 ) {
+    console.log('É opcional votar.')
+} else if (idade >= 18 && idade < 65) {
+    console.log('Você é obrigado a votar.')
+} else {console.log('É opcional votar.')}
+
+    
+
